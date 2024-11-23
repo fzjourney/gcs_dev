2c28695895f79402a09ab47cf8ef6f018f2f109e
Muhammad Feiza Zharfan Priyatnadi <100272742+fzjourney@users.noreply.github.com>
2024-11-23 21:31:27 +0700

    Dev into Main (#10)
    * Takeout zoom feature while video stream
    
    * Axis and movement need fix
    
    * Fix movement
    
    * Fix movement and axis
    
    * GCS-18 & GCS-19 (#9)
    
    * ui for gcs
    
    * Pulling
    
    * Fixing Movement Bug
    
    * Before changing class name fix
    
    * Changing based on class diagram -> MetricsSystem and Controller
    
    * Changing based on class diagram -> MetricsSystem and Controller
    
    * Changing based on class diagram -> MetricsSystem and Controller
    
    * Changing based on class diagram -> SoftwareGCS
    
    * Changing based on class diagram -> SoftwareGCS
    
    * Add Class CameraFilter
    
    * Fix bug camera filter, pull dev_qs_1
    
    * GCS-18 DAN GCS-19, Fix bug GCS-18 dan Pembuatan new class Log
    
    ---------
    
    Co-authored-by: QaisAlfarisi <qaisalfarisi@gmail.com>
    Co-authored-by: M Feiza <muhfeiza@gmail.com>
    
    ---------
    
    Co-authored-by: M Feiza <muhfeiza@gmail.com>
    Co-authored-by: QaisAlfarisi <qaisalfarisi@gmail.com>

---
+++ manager/CameraFilter.py
@@ -0,0 +1,22 @@
+import cv2
+
+class CameraFilter:
+    def __init__(self):
+        self.current_filter = "normal"  
+
+    def set_filter(self, filter_type):
+        self.current_filter = filter_type
+        
+    def apply_filter(self, frame):
+        if self.current_filter == "normal":
+            return frame
+        elif self.current_filter == "bw":
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frame = cv2.adaptiveThreshold(gray_frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+            return cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+        elif self.current_filter == "grayscale":
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            return cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+        elif self.current_filter == "invert":
+            frame = cv2.bitwise_not(frame)
+            return frame
